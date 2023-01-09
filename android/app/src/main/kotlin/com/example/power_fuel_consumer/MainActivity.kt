6fdd6f898ebc4e61fdb68de96fb513c33d18eeff
manojitht <manojith12345@outlook.com>
2023-01-09 16:32:40 +0530

    power fuel integrated with flutter app

---
+++ android/app/src/main/kotlin/com/example/power_fuel_consumer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.power_fuel_consumer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late WebViewController _controller;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFf88512),
+      body: SafeArea(
+        child: WebView(
+          initialUrl: 'http://env-samplepowerfuel.eba-hppy27ka.us-west-2.elasticbeanstalk.com/fs-admin',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController powerfuel){
+            _controller = powerfuel;
+          },
+        ),
+      ),
+    );
+}
+}
